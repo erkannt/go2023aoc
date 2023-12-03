c7a9750a703d358e73760418e7d2d0310b7fd9d1
Daniel Haarhoff <daniel@rknt.de>
2023-12-03 10:24:01 +0000

    Start sketching algorithm

--- 03/solution_test.go
+++ 03/solution_test.go
@@ -6,8 +6,34 @@ import (
 	"testing"
 )
 
-func ProblemOne(bufio.Scanner) int {
-	return 0
+type Location struct {
+	x int
+	y int
+}
+
+type Partnumber struct {
+	value    int
+	location Location
+	lenght   int
+}
+
+func parseSchematic(scanner bufio.Scanner) ([]Partnumber, []Location) {
+	return []Partnumber{}, []Location{}
+}
+
+func isAdjacent(number Partnumber, locations []Location) bool {
+	return false
+}
+
+func ProblemOne(scanner bufio.Scanner) int {
+	var total = 0
+	partNumbers, symbolLocations := parseSchematic(scanner)
+	for _, number := range partNumbers {
+		if isAdjacent(number, symbolLocations) {
+			total += number.value
+		}
+	}
+	return total
 }
 
 func TestProblemOne(t *testing.T) {
