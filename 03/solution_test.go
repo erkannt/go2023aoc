ab2d963cec3d1a3a8df366be23be3f20f89ae685
Daniel Haarhoff <daniel@rknt.de>
2023-12-03 11:06:55 +0000

    get example for part 1 to pass

--- 03/solution_test.go
+++ 03/solution_test.go
@@ -61,7 +61,7 @@ func parseSchematic(scanner bufio.Scanner) ([]Partnumber, []Location) {
 func isAdjacent(number Partnumber, locations []Location) bool {
 	var neighbours = []string{
 		fmt.Sprintf("%d%d", number.location.x-1, number.location.y),
-		fmt.Sprintf("%d%d", number.location.x+1, number.location.y),
+		fmt.Sprintf("%d%d", number.location.x+number.lenght, number.location.y),
 	}
 	for i := number.location.x - 1; i <= number.location.x+number.lenght+1; i++ {
 		neighbours = append(neighbours, fmt.Sprintf("%d%d", i, number.location.y-1))
