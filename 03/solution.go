548c68bdb7cdc54213f896883e701b4e6cf157ce
Daniel Haarhoff <daniel@rknt.de>
2023-12-03 11:15:00 +0000

    Avoid ambiguous comparison strings

--- 03/solution.go
+++ 03/solution.go
@@ -59,16 +59,16 @@ func parseSchematic(scanner bufio.Scanner) ([]Partnumber, []Location) {
 
 func isAdjacent(number Partnumber, locations []Location) bool {
 	var neighbours = []string{
-		fmt.Sprintf("%d%d", number.location.x-1, number.location.y),
-		fmt.Sprintf("%d%d", number.location.x+number.lenght, number.location.y),
+		fmt.Sprintf("%d:%d", number.location.x-1, number.location.y),
+		fmt.Sprintf("%d:%d", number.location.x+number.lenght, number.location.y),
 	}
 	for i := number.location.x - 1; i <= number.location.x+number.lenght+1; i++ {
-		neighbours = append(neighbours, fmt.Sprintf("%d%d", i, number.location.y-1))
-		neighbours = append(neighbours, fmt.Sprintf("%d%d", i, number.location.y+1))
+		neighbours = append(neighbours, fmt.Sprintf("%d:%d", i, number.location.y-1))
+		neighbours = append(neighbours, fmt.Sprintf("%d:%d", i, number.location.y+1))
 	}
 	for _, loc := range locations {
 		for _, neighbour := range neighbours {
-			if neighbour == fmt.Sprintf("%d%d", loc.x, loc.y) {
+			if neighbour == fmt.Sprintf("%d:%d", loc.x, loc.y) {
 				return true
 			}
 		}
