f0075cf248013c6dd6f76247c8b002abe4197158
Daniel Haarhoff <daniel@rknt.de>
2023-12-04 08:53:45 +0000

    Solve part 1

--- 04/solution_test.go
+++ 04/solution_test.go
@@ -3,51 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"reflect"
-	"regexp"
 	"strings"
 	"testing"
 )
-
-func parseCard(card string) ([]string, []string) {
-	sectionsRegex, _ := regexp.Compile("([0-9]+):([0-9 ]+)|([0-9 ]+)")
-	numberRegex, _ := regexp.Compile("[0-9]+")
-
-	sections := sectionsRegex.FindAllStringSubmatch(card, -1)
-	winningNumbers := numberRegex.FindAllString(sections[1][0], -1)
-	cardNumbers := numberRegex.FindAllString(sections[2][0], -1)
-	return winningNumbers, cardNumbers
-}
-
-func cardValue(matchCount int) int {
-	if matchCount == 0 {
-		return 0
-	}
-	value := math.Pow(2, float64(matchCount)-1)
-	return int(value)
-}
-
-func ProblemOne(scanner bufio.Scanner) int {
-	var total = 0
-	for scanner.Scan() {
-		line := scanner.Text()
-		if line == "" {
-			continue
-		}
-		winningNumbers, cardNumbers := parseCard(line)
-		var matches = []string{}
-		for _, cardNumber := range cardNumbers {
-			for _, winningNumber := range winningNumbers {
-				if cardNumber == winningNumber {
-					matches = append(matches, cardNumber)
-				}
-			}
-		}
-		total += cardValue(len(matches))
-	}
-	return total
-}
 
 func TestProblemOne(t *testing.T) {
 	input := `
