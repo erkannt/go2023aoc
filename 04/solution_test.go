1cf2b04edc6fe81695a8f77c05245c8f569ddc6d
Daniel Haarhoff <daniel@rknt.de>
2023-12-04 08:37:51 +0000

    Make sure cardValue implementation is correct #04

--- 04/solution_test.go
+++ 04/solution_test.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"math"
+	"reflect"
 	"regexp"
 	"strings"
 	"testing"
@@ -63,4 +65,26 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 		t.Errorf("\nExpected: %v\nResult: %v", expected, result)
 	}
 
+}
+
+func TestCardValue(t *testing.T) {
+	cases := []struct {
+		Input    int
+		Expected int
+	}{
+		{0, 0},
+		{1, 1},
+		{2, 2},
+		{3, 4},
+		{4, 8},
+	}
+	for _, testCase := range cases {
+		t.Run(fmt.Sprintf("%v -> %v", testCase.Input, testCase.Expected), func(t *testing.T) {
+			result := cardValue(testCase.Input)
+			if !reflect.DeepEqual(result, testCase.Expected) {
+				t.Errorf("actual: %v", result)
+			}
+		})
+
+	}
 }
