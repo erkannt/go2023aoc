300293e50580ca3912d020cfb7beacb91be2604a
Daniel Haarhoff <daniel@rknt.de>
2023-12-06 07:22:21 +0000

    Solve example part 1 #06

--- 06/solution_test.go
+++ 06/solution_test.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"math"
+	"regexp"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -17,25 +21,48 @@ type Race struct {
 }
 
 func parseRaces(scanner bufio.Scanner) []Race {
-	return []Race{}
+	numberRegex, _ := regexp.Compile("[0-9]+")
+	races := []Race{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "Time: ") {
+			numbers := numberRegex.FindAllString(line, -1)
+			for _, number := range numbers {
+				value, _ := strconv.Atoi(number)
+				races = append(races, Race{duration: value})
+			}
+		}
+		if strings.HasPrefix(line, "Distance: ") {
+			numbers := numberRegex.FindAllString(line, -1)
+			for i, number := range numbers {
+				value, _ := strconv.Atoi(number)
+				races[i].record = value
+			}
+		}
+	}
+	return races
 }
 
 func optimiseForRace(race Race) (int, int) {
-	return 0, 0
+	durationSquared := math.Pow(float64(race.duration), 2)
+	minButton := math.Floor(float64(race.duration)/2 - math.Sqrt(durationSquared/4-float64(race.record)))
+	maxButton := math.Ceil(float64(race.duration)/2+math.Sqrt(durationSquared/4-float64(race.record))) - 1
+	return int(minButton), int(maxButton)
 }
 
 func ProblemOne(scanner bufio.Scanner) int {
-	total := 0
+	total := 1
 	races := parseRaces(scanner)
 	for _, race := range races {
 		minButton, maxButton := optimiseForRace(race)
+		fmt.Printf("%v %v %v\n", race, minButton, maxButton)
 		total *= maxButton - minButton
 	}
 	return total
 }
 
 func TestProblemOne(t *testing.T) {
-	expected := 35
+	expected := 288
 
 	result := ProblemOne(*bufio.NewScanner(strings.NewReader(problemInput)))
 	if result != expected {
