b99f1220dd57e987a664b32caea48cfe0377e070
Daniel Haarhoff <daniel@rknt.de>
2023-12-07 12:52:49 +0000

    First stab at part 2 #07

--- 07/solution.go
+++ 07/solution.go
@@ -57,8 +57,78 @@ func getType(cards string) CardsType {
 	}
 }
 
-func cardValue(input byte) int {
-	cardsByValue := "23456789TJQKA"
+func getTypeWithJoker(cards string) CardsType {
+	uniqueCards := map[rune]int{}
+	for _, v := range cards {
+		uniqueCards[v] += 1
+	}
+	counts := []int{}
+	for _, v := range uniqueCards {
+		counts = append(counts, v)
+	}
+	slices.Sort(counts)
+	slices.Reverse(counts)
+	switch {
+	case reflect.DeepEqual(counts, []int{5}):
+		return FiveOfAKind
+	case reflect.DeepEqual(counts, []int{4, 1}):
+		switch uniqueCards['J'] {
+		case 1:
+			return FiveOfAKind
+		default:
+			return FourOfAKind
+		}
+	case reflect.DeepEqual(counts, []int{3, 2}):
+		switch uniqueCards['J'] {
+		case 3:
+			return FiveOfAKind
+		case 2:
+			return FiveOfAKind
+		default:
+			return FullHouse
+		}
+	case reflect.DeepEqual(counts, []int{3, 1, 1}):
+		switch uniqueCards['J'] {
+		case 3:
+			return FourOfAKind
+		case 1:
+			return FourOfAKind
+		default:
+			return ThreeOfAKind
+		}
+	case reflect.DeepEqual(counts, []int{2, 2, 1}):
+		switch uniqueCards['J'] {
+		case 2:
+			return FourOfAKind
+		case 1:
+			return FullHouse
+		default:
+			return TwoPair
+		}
+	case reflect.DeepEqual(counts, []int{2, 1, 1, 1}):
+		switch uniqueCards['J'] {
+		case 2:
+			return ThreeOfAKind
+		case 1:
+			return ThreeOfAKind
+		default:
+			return OnePair
+		}
+	default:
+		switch uniqueCards['J'] {
+		case 1:
+			return OnePair
+		default:
+			return HighCard
+		}
+	}
+}
+
+const partOneValues = "23456789TJQKA"
+
+const partTwoValues = "J23456789TQKA"
+
+func cardValue(input byte, cardsByValue string) int {
 	for i := 0; i < len(cardsByValue); i++ {
 		if cardsByValue[i] == input {
 			return i
@@ -67,10 +137,10 @@ func cardValue(input byte) int {
 	return -1
 }
 
-func firstDifferentCardIsLower(cardsA, cardsB string) bool {
+func firstDifferentCardIsLower(cardsA, cardsB, cardsByValue string) bool {
 	for i := 0; i <= 4; i++ {
-		valueA := cardValue(cardsA[i])
-		valueB := cardValue(cardsB[i])
+		valueA := cardValue(cardsA[i], cardsByValue)
+		valueB := cardValue(cardsB[i], cardsByValue)
 		if valueA > valueB {
 			return false
 		}
@@ -96,7 +166,26 @@ func (a ByRank) Less(i, j int) bool {
 	case cardsTypeI > cardsTypeJ:
 		return false
 	default:
-		return firstDifferentCardIsLower(cardsI, cardsJ)
+		return firstDifferentCardIsLower(cardsI, cardsJ, partOneValues)
+	}
+}
+
+type ByRankWithJoker []Hand
+
+func (a ByRankWithJoker) Len() int      { return len(a) }
+func (a ByRankWithJoker) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ByRankWithJoker) Less(i, j int) bool {
+	cardsI := a[i].cards
+	cardsJ := a[j].cards
+	cardsTypeI := getTypeWithJoker(cardsI)
+	cardsTypeJ := getTypeWithJoker(cardsJ)
+	switch {
+	case cardsTypeI < cardsTypeJ:
+		return true
+	case cardsTypeI > cardsTypeJ:
+		return false
+	default:
+		return firstDifferentCardIsLower(cardsI, cardsJ, partTwoValues)
 	}
 }
 
@@ -133,6 +222,12 @@ func ProblemOne(scanner bufio.Scanner) int {
 	return tally(hands)
 }
 
+func ProblemTwo(scanner bufio.Scanner) int {
+	hands := parseHands(scanner)
+	sort.Sort(ByRankWithJoker(hands))
+	return tally(hands)
+}
+
 func main() {
 	file, err := os.Open("./input.txt")
 	if err != nil {
@@ -141,4 +236,6 @@ func main() {
 	defer file.Close()
 
 	println("Problem1:", ProblemOne(*bufio.NewScanner(file)))
+	file.Seek(0, 0)
+	println("Problem2:", ProblemTwo(*bufio.NewScanner(file)))
 }
