74769fe00a6d8c1c42111bb7d95912fb68bf96c0
Daniel Haarhoff <haarhoff@mailbox.org>
2023-12-08 09:31:39 +0000

    solve part 1 #08

--- 08/solution.go
+++ 08/solution.go
@@ -4,11 +4,42 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 )
 
+type Node map[rune]string
+
 func ProblemOne(scanner bufio.Scanner) int {
-	total := 1
-	return total
+	instructions := []rune{}
+	network := make(map[string]Node)
+	nodeRegex := regexp.MustCompile("([A-Z]{3}).*([A-Z]{3}), ([A-Z]{3})")
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+		if strings.Contains(line, "=") {
+			matches := nodeRegex.FindAllStringSubmatch(line, -1)
+			node := matches[0][1]
+			left := matches[0][2]
+			right := matches[0][3]
+			network[node] = Node{'L': left, 'R': right}
+			continue
+		}
+		instructions = []rune(line)
+	}
+	step := 0
+	current := "AAA"
+	for {
+		instr := instructions[step%len(instructions)]
+		current = network[current][instr]
+		if current == "ZZZ" {
+			break
+		}
+		step++
+	}
+	return step + 1
 }
 
 func main() {
