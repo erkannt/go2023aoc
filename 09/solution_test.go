1ce29f64449cc266ee5e0c936aab68ae2fdfea30
Daniel Haarhoff <daniel@rknt.de>
2023-12-09 12:33:56 +0000

    Parse the input #09

--- 09/solution_test.go
+++ 09/solution_test.go
@@ -2,11 +2,36 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 	"testing"
 )
 
+func parseHistories(scanner bufio.Scanner) [][]int {
+	numbersRegex := regexp.MustCompile("[-0-9]+")
+	histories := [][]int{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+		asStrings := numbersRegex.FindAllString(line, -1)
+		numbers := []int{}
+		for _, str := range asStrings {
+			value, _ := strconv.Atoi(str)
+			numbers = append(numbers, value)
+		}
+		fmt.Printf("%v\n", numbers)
+		histories = append(histories, numbers)
+	}
+	return histories
+}
+
 func ProblemOne(scanner bufio.Scanner) int {
+	histories := parseHistories(scanner)
+	fmt.Printf("%v\n", histories)
 	return 0
 }
 
