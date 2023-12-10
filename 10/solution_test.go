01c4b19c76635d72763defa691550bf3d1f9bd9b
Daniel Haarhoff <daniel@rknt.de>
2023-12-10 10:10:36 +0000

    Sketch algorithm #10

--- 10/solution_test.go
+++ 10/solution_test.go
@@ -1,22 +1,68 @@
 package main
 
 import (
+	"bufio"
+	"strings"
 	"testing"
 )
 
-func ProblemOne(input string) int {
-	return 0
+type Position struct {
+	x int
+	y int
+}
+
+type Step struct {
+	x int
+	y int
+}
+
+func findStart(mapLines [][]rune) Position {
+	return Position{}
+}
+
+func findFirstStep(mapLines [][]rune, startPos Position) Step {
+	return Step{}
+}
+
+func getNextStep(prevStep Step, pipeshape rune) Step {
+	return Step{}
+}
+
+func getPipeShape(pos Position, maplines [][]rune) rune {
+	return '|'
+}
+
+func ProblemOne(scanner bufio.Scanner) int {
+	mapLines := [][]rune{}
+	for scanner.Scan() {
+		mapLines = append(mapLines, []rune(scanner.Text()))
+	}
+	pipeLength := 0
+	startPos := findStart(mapLines)
+	currentPos := findStart(mapLines)
+	step := findFirstStep(mapLines, currentPos)
+	for {
+		currentPos.x += step.x
+		currentPos.y += step.y
+		if currentPos.x == startPos.x && currentPos.y == startPos.y {
+			break
+		}
+		step = getNextStep(step, getPipeShape(currentPos, mapLines))
+		pipeLength++
+	}
+	return pipeLength
 }
 
 func TestProblemOne(t *testing.T) {
-	input := `.....
+	input := `
+.....
 .S-7.
 .|.|.
 .L-J.
 .....
 `
 	expected := 4
-	actual := ProblemOne(input)
+	actual := ProblemOne(*bufio.NewScanner(strings.NewReader(input)))
 	if actual != expected {
 		t.Errorf("Expected: %v\nActual: %v\n", expected, actual)
 	}
