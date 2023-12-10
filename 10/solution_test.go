159ab39d86a29ed5768c48621c975f2ee109798d
Daniel Haarhoff <daniel@rknt.de>
2023-12-10 10:45:48 +0000

    Solve part 1 example #10

--- 10/solution_test.go
+++ 10/solution_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"log"
+	"slices"
 	"strings"
 	"testing"
 )
@@ -17,19 +20,66 @@ type Step struct {
 }
 
 func findStart(mapLines [][]rune) Position {
+	for y, line := range mapLines {
+		for x, r := range line {
+			if r == 'S' {
+				return Position{x: x, y: y}
+			}
+		}
+	}
+	log.Fatal("Not start found")
 	return Position{}
 }
 
 func findFirstStep(mapLines [][]rune, startPos Position) Step {
+	switch {
+	case slices.Contains([]rune{'-', 'L', 'F'}, mapLines[startPos.y][startPos.x-1]):
+		return Step{x: -1, y: 0}
+	case slices.Contains([]rune{'-', 'J', '7'}, mapLines[startPos.y][startPos.x+1]):
+		return Step{x: 1, y: 0}
+	case slices.Contains([]rune{'|', 'J', 'L'}, mapLines[startPos.y+1][startPos.x]):
+		return Step{x: 0, y: 1}
+	case slices.Contains([]rune{'|', 'F', '7'}, mapLines[startPos.y-1][startPos.x]):
+		return Step{x: 0, y: -1}
+	}
+	log.Fatal("No valid first step found")
 	return Step{}
 }
 
 func getNextStep(prevStep Step, pipeshape rune) Step {
+	log.Printf("%v %v\n", prevStep, string(pipeshape))
+	switch pipeshape {
+	case '|':
+		return Step{x: 0, y: prevStep.y}
+	case '-':
+		return Step{x: prevStep.x, y: 0}
+	case 'L':
+		if prevStep.y == 1 {
+			return Step{x: 1, y: 0}
+		}
+		return Step{x: 0, y: -1}
+	case 'J':
+		if prevStep.y == 1 {
+			return Step{x: -1, y: 0}
+		}
+		return Step{x: 0, y: -1}
+	case '7':
+		if prevStep.y == -1 {
+			return Step{x: -1, y: 0}
+		}
+		return Step{x: 0, y: 1}
+	case 'F':
+		if prevStep.y == -1 {
+			return Step{x: 1, y: 0}
+		}
+		return Step{x: 0, y: 1}
+	}
+	log.Fatal("Invalid pipeshape")
 	return Step{}
 }
 
 func getPipeShape(pos Position, maplines [][]rune) rune {
-	return '|'
+	return maplines[pos.y][pos.x]
 }
 
 func ProblemOne(scanner bufio.Scanner) int {
@@ -44,13 +94,14 @@ func ProblemOne(scanner bufio.Scanner) int {
 	for {
 		currentPos.x += step.x
 		currentPos.y += step.y
+		fmt.Printf("%v  ", currentPos)
 		if currentPos.x == startPos.x && currentPos.y == startPos.y {
 			break
 		}
 		step = getNextStep(step, getPipeShape(currentPos, mapLines))
 		pipeLength++
 	}
-	return pipeLength
+	return pipeLength/2 + 1
 }
 
 func TestProblemOne(t *testing.T) {
