b78c8320ff907a9a8dd078b413e2769c53fd0494
Daniel Haarhoff <daniel@rknt.de>
2023-12-10 11:43:07 +0000

    Failing test for part 2 #10

---
+++ 10/solution.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"slices"
+)
+
+type Position struct {
+	x int
+	y int
+}
+
+type Step struct {
+	x int
+	y int
+}
+
+func findStart(mapLines [][]rune) Position {
+	for y, line := range mapLines {
+		for x, r := range line {
+			if r == 'S' {
+				return Position{x: x, y: y}
+			}
+		}
+	}
+	log.Fatal("Not start found")
+	return Position{}
+}
+
+func findFirstStep(mapLines [][]rune, startPos Position) Step {
+	switch {
+	case slices.Contains([]rune{'-', 'L', 'F'}, mapLines[startPos.y][startPos.x-1]):
+		return Step{x: -1, y: 0}
+	case slices.Contains([]rune{'-', 'J', '7'}, mapLines[startPos.y][startPos.x+1]):
+		return Step{x: 1, y: 0}
+	case slices.Contains([]rune{'|', 'J', 'L'}, mapLines[startPos.y+1][startPos.x]):
+		return Step{x: 0, y: 1}
+	case slices.Contains([]rune{'|', 'F', '7'}, mapLines[startPos.y-1][startPos.x]):
+		return Step{x: 0, y: -1}
+	}
+	log.Fatal("No valid first step found")
+	return Step{}
+}
+
+func getNextStep(prevStep Step, pipeshape rune) Step {
+	log.Printf("%v %v\n", prevStep, string(pipeshape))
+	switch pipeshape {
+	case '|':
+		return Step{x: 0, y: prevStep.y}
+	case '-':
+		return Step{x: prevStep.x, y: 0}
+	case 'L':
+		if prevStep.y == 1 {
+			return Step{x: 1, y: 0}
+		}
+		return Step{x: 0, y: -1}
+	case 'J':
+		if prevStep.y == 1 {
+			return Step{x: -1, y: 0}
+		}
+		return Step{x: 0, y: -1}
+	case '7':
+		if prevStep.y == -1 {
+			return Step{x: -1, y: 0}
+		}
+		return Step{x: 0, y: 1}
+	case 'F':
+		if prevStep.y == -1 {
+			return Step{x: 1, y: 0}
+		}
+		return Step{x: 0, y: 1}
+	}
+	log.Fatal("Invalid pipeshape")
+	return Step{}
+}
+
+func getPipeShape(pos Position, maplines [][]rune) rune {
+	return maplines[pos.y][pos.x]
+}
+
+func ProblemOne(scanner bufio.Scanner) int {
+	mapLines := [][]rune{}
+	for scanner.Scan() {
+		mapLines = append(mapLines, []rune(scanner.Text()))
+	}
+	pipeLength := 0
+	startPos := findStart(mapLines)
+	currentPos := findStart(mapLines)
+	step := findFirstStep(mapLines, currentPos)
+	for {
+		currentPos.x += step.x
+		currentPos.y += step.y
+		fmt.Printf("%v  ", currentPos)
+		if currentPos.x == startPos.x && currentPos.y == startPos.y {
+			break
+		}
+		step = getNextStep(step, getPipeShape(currentPos, mapLines))
+		pipeLength++
+	}
+	return pipeLength/2 + 1
+}
+
+func ProblemTwo(scanner bufio.Scanner) int {
+	return 0
+}
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	println("Problem1:", ProblemOne(*bufio.NewScanner(file)))
+	file.Seek(0, 0)
+	println("Problem2:", ProblemTwo(*bufio.NewScanner(file)))
+}
--- 10/solution_test.go
+++ 10/solution_test.go
@@ -2,107 +2,9 @@ package main
 
 import (
 	"bufio"
-	"fmt"
-	"log"
-	"slices"
 	"strings"
 	"testing"
 )
-
-type Position struct {
-	x int
-	y int
-}
-
-type Step struct {
-	x int
-	y int
-}
-
-func findStart(mapLines [][]rune) Position {
-	for y, line := range mapLines {
-		for x, r := range line {
-			if r == 'S' {
-				return Position{x: x, y: y}
-			}
-		}
-	}
-	log.Fatal("Not start found")
-	return Position{}
-}
-
-func findFirstStep(mapLines [][]rune, startPos Position) Step {
-	switch {
-	case slices.Contains([]rune{'-', 'L', 'F'}, mapLines[startPos.y][startPos.x-1]):
-		return Step{x: -1, y: 0}
-	case slices.Contains([]rune{'-', 'J', '7'}, mapLines[startPos.y][startPos.x+1]):
-		return Step{x: 1, y: 0}
-	case slices.Contains([]rune{'|', 'J', 'L'}, mapLines[startPos.y+1][startPos.x]):
-		return Step{x: 0, y: 1}
-	case slices.Contains([]rune{'|', 'F', '7'}, mapLines[startPos.y-1][startPos.x]):
-		return Step{x: 0, y: -1}
-	}
-	log.Fatal("No valid first step found")
-	return Step{}
-}
-
-func getNextStep(prevStep Step, pipeshape rune) Step {
-	log.Printf("%v %v\n", prevStep, string(pipeshape))
-	switch pipeshape {
-	case '|':
-		return Step{x: 0, y: prevStep.y}
-	case '-':
-		return Step{x: prevStep.x, y: 0}
-	case 'L':
-		if prevStep.y == 1 {
-			return Step{x: 1, y: 0}
-		}
-		return Step{x: 0, y: -1}
-	case 'J':
-		if prevStep.y == 1 {
-			return Step{x: -1, y: 0}
-		}
-		return Step{x: 0, y: -1}
-	case '7':
-		if prevStep.y == -1 {
-			return Step{x: -1, y: 0}
-		}
-		return Step{x: 0, y: 1}
-	case 'F':
-		if prevStep.y == -1 {
-			return Step{x: 1, y: 0}
-		}
-		return Step{x: 0, y: 1}
-	}
-	log.Fatal("Invalid pipeshape")
-	return Step{}
-}
-
-func getPipeShape(pos Position, maplines [][]rune) rune {
-	return maplines[pos.y][pos.x]
-}
-
-func ProblemOne(scanner bufio.Scanner) int {
-	mapLines := [][]rune{}
-	for scanner.Scan() {
-		mapLines = append(mapLines, []rune(scanner.Text()))
-	}
-	pipeLength := 0
-	startPos := findStart(mapLines)
-	currentPos := findStart(mapLines)
-	step := findFirstStep(mapLines, currentPos)
-	for {
-		currentPos.x += step.x
-		currentPos.y += step.y
-		fmt.Printf("%v  ", currentPos)
-		if currentPos.x == startPos.x && currentPos.y == startPos.y {
-			break
-		}
-		step = getNextStep(step, getPipeShape(currentPos, mapLines))
-		pipeLength++
-	}
-	return pipeLength/2 + 1
-}
 
 func TestProblemOne(t *testing.T) {
 	input := `.....
@@ -114,6 +16,20 @@ func TestProblemOne(t *testing.T) {
 	expected := 4
 	actual := ProblemOne(*bufio.NewScanner(strings.NewReader(input)))
 	if actual != expected {
-		t.Errorf("Expected: %v\nActual: %v\n", expected, actual)
+		t.Errorf("\nExpected: %v\nActual: %v\n", expected, actual)
+	}
+}
+
+func TestProblemTwo(t *testing.T) {
+	input := `.....
+.S-7.
+.|.|.
+.L-J.
+.....
+`
+	expected := 1
+	actual := ProblemTwo(*bufio.NewScanner(strings.NewReader(input)))
+	if actual != expected {
+		t.Errorf("\nExpected: %v\nActual: %v\n", expected, actual)
 	}
 }
