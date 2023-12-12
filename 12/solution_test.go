a167a829c485343896cbe2f24b9089d3418eb1e5
Daniel Haarhoff <daniel@rknt.de>
2023-12-12 10:05:55 +0000

    Parsing #12

--- 12/solution_test.go
+++ 12/solution_test.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func parse(input string) (string, []int) {
+	numberRegex := regexp.MustCompile("[0-9]+")
+	parts := strings.Split(input, " ")
+	groupsStrings := numberRegex.FindAllStringSubmatch(parts[1], -1)
+	groups := []int{}
+	for _, v := range groupsStrings {
+		value, _ := strconv.Atoi(v[0])
+		groups = append(groups, value)
+	}
+	return parts[0], groups
+}
+
 func PossibleArrangements(input string) int {
+	record, groups := parse(input)
+	fmt.Printf("%v %v", record, groups)
 	return 0
 }
 
