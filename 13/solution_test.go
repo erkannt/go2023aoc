353d835e151ef0463ab7031db825c569233e82d0
Daniel Haarhoff <daniel@rknt.de>
2023-12-13 09:11:49 +0000

    Parse input #13

--- 13/solution_test.go
+++ 13/solution_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"strings"
 	"testing"
 
@@ -9,6 +10,34 @@ import (
 )
 
 func ProblemOne(scanner bufio.Scanner) int {
+	orig := []string{}
+	flipped := []string{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			fmt.Println("orig")
+			for _, v := range orig {
+				fmt.Printf("%v\n", v)
+			}
+			fmt.Println("flipped")
+			for _, v := range flipped {
+				fmt.Printf("%v\n", v)
+			}
+			orig = []string{}
+			flipped = []string{}
+			continue
+		}
+
+		orig = append(orig, line)
+		if len(flipped) == 0 {
+			for range line {
+				flipped = append(flipped, "")
+			}
+		}
+		for i, r := range line {
+			flipped[i] += string(r)
+		}
+	}
 	return 0
 }
 
